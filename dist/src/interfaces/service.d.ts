f4371fc8c5922fc85563627d785c74b6ca03f976
csskevin <saigerkevin4@gmail.com>
2020-03-20 23:10:23 +0100

    Updating service interface.

--- dist/src/interfaces/service.d.ts
+++ dist/src/interfaces/service.d.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 interface Service {
     method: "checkout" | "copy" | "delete" | "get" | "head" | "lock" | "merge" | "mkactivity" | "mkcol" | "move" | "m-search" | "notify" | "options" | "patch" | "post" | "purge" | "put" | "report" | "search" | "subscribe" | "trace" | "unlock" | "unsubscribe";
     path: string;
-    parser?: Function;
+    parser?: any;
     handler(req: Request, res: Response): any;
 }
 export default Service;
--- src/interfaces/service.ts
+++ src/interfaces/service.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 interface Service { 
     method: "checkout" | "copy" | "delete" | "get" | "head" | "lock" | "merge" | "mkactivity" | "mkcol" | "move" | "m-search" | "notify" | "options" | "patch" | "post" | "purge" | "put" | "report" | "search" | "subscribe" | "trace" | "unlock" | "unsubscribe", 
     path: string,
-    parser?: Function,
+    parser?: any,
     handler(req: Request, res: Response): any
 }
 
